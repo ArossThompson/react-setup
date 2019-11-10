590a9a1a06ffa6973a38e88b3f7093b49e5d0b39
ross thompson <rossathompson94@gmail.com>
2019-11-10 16:25:36 +0000

    finished configuration to allow SASS compiling and Jest and Enzyme Testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({ adapter: new Adapter() });
+
+test('It renders a div without errors', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find('#main-div').length).toBe(1);
+})
+
